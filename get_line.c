05aaf4288a0499d7058a692d14ee930c042de758
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-11-28 09:35:33 -0600

    update getline file

--- get_line.c
+++ get_line.c
@@ -7,7 +7,7 @@
 char *_getline()
 {
 int i, buffsize = BUFSIZE, rd;
-char c = 0;
+signed char c = 0;
 char *buff = malloc(buffsize);
 
 	if (buff == NULL)
@@ -16,7 +16,7 @@ char *buff = malloc(buffsize);
 		return (NULL);
 	}
 
-	for (i = 0; c >= EOF && c != '\n'; i++)
+	for (i = 0; c != EOF && c != '\n'; i++)
 	{
 		fflush(stdin);
 		rd = read(STDIN_FILENO, &c, 1);
