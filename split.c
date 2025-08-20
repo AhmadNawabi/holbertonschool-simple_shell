6c0c95e99beef41d166b99c9a809468135033e78
Ahmad Nawabi <106168136+AhmadNawabi@users.noreply.github.com>
2025-08-20 09:29:09 -0500

    Cleanup: Refactored read_line.c and split.c with Betty style and doc comments

--- split.c
+++ split.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * swap_char - swaps | and & for non-printed chars
+ * swap_char - Swaps '|' and '&' with non-printed chars
+ * @input: Input string
+ * @bool: Swap type (0 = encode, 1 = decode)
  *
- * @input: input string
- * @bool: type of swap
- * Return: swapped string
+ * Description:
+ * Used internally to distinguish single '|' and '&'
+ * characters from their logical forms '||' and '&&'.
+ *
+ * Return: Pointer to the modified string.
  */
 char *swap_char(char *input, int bool)
 {
@@ -44,12 +48,16 @@ char *swap_char(char *input, int bool)
 }
 
 /**
- * add_nodes - add separators and command lines in the lists
+ * add_nodes - Adds separators and commands to linked lists
+ * @head_s: Head of separator list
+ * @head_l: Head of command line list
+ * @input: Input string
  *
- * @head_s: head of separator list
- * @head_l: head of command lines list
- * @input: input string
- * Return: no return
+ * Description:
+ * Parses the input string, storing separators (';', '|', '&')
+ * in one list and command lines in another.
+ *
+ * Return: Nothing.
  */
 void add_nodes(sep_list **head_s, line_list **head_l, char *input)
 {
@@ -76,26 +84,25 @@ void add_nodes(sep_list **head_s, line_list **head_l, char *input)
 		add_line_node_end(head_l, line);
 		line = _strtok(NULL, ";|&");
 	} while (line != NULL);
-
 }
 
 /**
- * go_next - go to the next command line stored
+ * go_next - Moves to the next command line in list
+ * @list_s: Pointer to separator list
+ * @list_l: Pointer to command line list
+ * @datash: Pointer to data_shell structure
  *
- * @list_s: separator list
- * @list_l: command line list
- * @datash: data structure
- * Return: no return
+ * Description:
+ * Determines the next command to execute based on
+ * logical separators (&&, ||, ;) and the last status.
+ *
+ * Return: Nothing.
  */
 void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
 {
-	int loop_sep;
-	sep_list *ls_s;
-	line_list *ls_l;
-
-	loop_sep = 1;
-	ls_s = *list_s;
-	ls_l = *list_l;
+	int loop_sep = 1;
+	sep_list *ls_s = *list_s;
+	line_list *ls_l = *list_l;
 
 	while (ls_s != NULL && loop_sep)
 	{
@@ -104,14 +111,20 @@ void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
 			if (ls_s->separator == '&' || ls_s->separator == ';')
 				loop_sep = 0;
 			if (ls_s->separator == '|')
-				ls_l = ls_l->next, ls_s = ls_s->next;
+			{
+				ls_l = ls_l->next;
+				ls_s = ls_s->next;
+			}
 		}
 		else
 		{
 			if (ls_s->separator == '|' || ls_s->separator == ';')
 				loop_sep = 0;
 			if (ls_s->separator == '&')
-				ls_l = ls_l->next, ls_s = ls_s->next;
+			{
+				ls_l = ls_l->next;
+				ls_s = ls_s->next;
+			}
 		}
 		if (ls_s != NULL && !loop_sep)
 			ls_s = ls_s->next;
@@ -122,22 +135,21 @@ void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
 }
 
 /**
- * split_commands - splits command lines according to
- * the separators ;, | and &, and executes them
+ * split_commands - Splits command lines by separators
+ * @datash: Pointer to data_shell structure
+ * @input: Input string
  *
- * @datash: data structure
- * @input: input string
- * Return: 0 to exit, 1 to continue
+ * Description:
+ * Splits the input string using separators (;, |, &),
+ * stores them in lists, and executes each command line.
+ *
+ * Return: 0 if "exit" is called, 1 otherwise.
  */
 int split_commands(data_shell *datash, char *input)
 {
-
-	sep_list *head_s, *list_s;
-	line_list *head_l, *list_l;
+	sep_list *head_s = NULL, *list_s;
+	line_list *head_l = NULL, *list_l;
 	int loop;
-
-	head_s = NULL;
-	head_l = NULL;
 
 	add_nodes(&head_s, &head_l, input);
 
@@ -165,24 +177,28 @@ int split_commands(data_shell *datash, char *input)
 
 	if (loop == 0)
 		return (0);
+
 	return (1);
 }
 
 /**
- * split_line - tokenizes the input string
+ * split_line - Tokenizes the input string into arguments
+ * @input: Input string
  *
- * @input: input string.
- * Return: string splitted.
+ * Description:
+ * Splits the input string into an array of tokens (arguments)
+ * using delimiters defined in TOK_DELIM.
+ *
+ * Return: Array of tokenized strings.
  */
 char **split_line(char *input)
 {
-	size_t bsize;
+	size_t bsize = TOK_BUFSIZE;
 	size_t i;
 	char **tokens;
 	char *token;
 
-	bsize = TOK_BUFSIZE;
-	tokens = malloc(sizeof(char *) * (bsize));
+	tokens = malloc(sizeof(char *) * bsize);
 	if (tokens == NULL)
 	{
 		write(STDERR_FILENO, ": allocation error\n", 18);
